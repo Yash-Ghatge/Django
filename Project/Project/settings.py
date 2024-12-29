70d54bd22e401b975b2d6d6fa114c52dd877b73c
Yash-Ghatge <yashghatge012@gmail.com>
2024-12-29 19:57:21 +0530

    Django File

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Project/Project/views.py
+++ Project/Project/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def Home(request):
-    return HttpResponse("Hello! Your at HomePage")
+    #return HttpResponse("Hello! Your at HomePage")
+    return render(request,'index.html')
 
 def About(request):
     return HttpResponse("Hello! Your at About Page")
