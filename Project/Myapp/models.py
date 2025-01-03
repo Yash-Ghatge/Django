d0396ec0fcb7760981e2483e4955aba9e4d51a18
Yash-Ghatge <yashghatge012@gmail.com>
2025-01-03 20:33:20 +0530

    Django Files

--- Project/Myapp/models.py
+++ Project/Myapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Hotel(models.Model):
+    menu =[
+        ('P','Pizaa'),
+        ('T','Tea'),
+        ('C','Cake'),
+        ('D','Dosa'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='apps/')
+    date = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2,choices=menu)
+
+    def __str__(self):
+        return self.name
