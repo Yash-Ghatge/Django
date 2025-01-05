caba12ddcea0ddc9d5714a3b86e070225a05d7ff
Yash-Ghatge <yashghatge012@gmail.com>
2025-01-05 20:27:12 +0530

    Django Files

--- Project/Myapp/urls.py
+++ Project/Myapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [ 
     path('',views.Auth,name='Auth'),
+    path('<int:order_id>/',views.Details,name='Details')
 ]
--- Project/Myapp/views.py
+++ Project/Myapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Hotel
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def Auth(request):
     orders = Hotel.objects.all()
     return render(request,'Myapp/index.html',{'orders':orders})
+
+def Details(request,order_id):
+    order = get_object_or_404(Hotel,pk=order_id)
+    return render(request,'Myapp/all_index.html',{'order':order})
