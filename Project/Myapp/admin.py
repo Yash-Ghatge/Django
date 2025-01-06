3c3d5f94118124ac354bce4445ce2a1c7d0e0807
Yash-Ghatge <yashghatge012@gmail.com>
2025-01-06 20:38:24 +0530

    Django Files

--- Project/Myapp/admin.py
+++ Project/Myapp/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import Hotel
+from .models import Hotel,HotelReview,Customer,Certificate
 # Register your models here.
 
-admin.site.register(Hotel)
+class HotelReviewAdmin(admin.TabularInline):
+    model = HotelReview
+    extra = 2
+
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('name','date','type')
+    inlines = [HotelReviewAdmin]
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('customer_name','Bill','order')
+    filter_horizontal = ('customer_opinion',)
+
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = ('certificate_name','issued_date')
+
+
+admin.site.register(Hotel,HotelAdmin)
+admin.site.register(Customer,CustomerAdmin)
+admin.site.register(Certificate,CertificateAdmin)
--- Project/Myapp/models.py
+++ Project/Myapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Hotel(models.Model):
@@ -16,3 +17,32 @@ class Hotel(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class HotelReview(models.Model):
+    hotel = models.ForeignKey(Hotel,on_delete=models.CASCADE,related_name='hotelReview')
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    rate = models.IntegerField()
+    review = models.TextField()
+    date_add = models.DateTimeField(timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} review the {self.hotel.name}'
+    
+class Customer(models.Model):
+    customer_name = models.CharField(max_length=100)
+    order = models.CharField(max_length=100)
+    Bill = models.IntegerField()
+    customer_opinion = models.ManyToManyField(Hotel,related_name='customers')
+
+    def __str__(self):
+        return f'{self.customer_name} have dinner in {self.customer_opinion.name}'
+
+class Certificate(models.Model):
+    certificate = models.OneToOneField(Hotel,on_delete=models.CASCADE,related_name='certificate')
+    certificate_name = models.CharField(max_length=100)
+    issued_date = models.DateTimeField(timezone.now)
+    expired_date = models.DateTimeField(timezone.now)
+
+    def __str__(self):
+        return f'{self.certificate_name} is issued for {self.certificate.name}'
